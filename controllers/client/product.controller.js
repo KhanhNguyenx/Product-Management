5d40e2f63ddd1490a8ee8a6c65f89d3bf35fd4dd
KhanhNguyenx <nmkhanh121203@gmail.com>
2025-01-07 23:31:26 +0700

    Danh sách sản phẩm bên client (Cả sản phẩm danh mục con)

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,7 @@
 const productsHelper = require("../../helpers/products");
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
+const productCategoryHelper = require("../../helpers/product-category");
 // [GET]/products
 module.exports.index = async (req, res) => {
   const products = await Product.find({
@@ -32,4 +34,36 @@ module.exports.detail = async (req, res) => {
     req.flash("error", `Không tồn tại sản phẩm`);
     res.redirect(`/products`);
   }
+};
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+  const slugCategory = req.params.slugCategory;
+
+  const category = await ProductCategory.findOne({
+    slug: slugCategory,
+    status: "active",
+    deleted: false,
+  });
+
+  const listSubCategory = await productCategoryHelper.getSubCategory(
+    category.id
+  );
+
+  const listSubCategoryId = listSubCategory.map((item) => item.id);
+
+  const products = await Product.find({
+    product_category_id: { $in: [category.id, ...listSubCategoryId] },
+    status: "active",
+    deleted: false,
+  }).sort({ position: "desc" });
+
+  const newProducts = productsHelper.priceNewProducts(products);
+
+  console.log(products);
+
+  res.render("client/pages/products/index", {
+    pageTitle: category.title,
+    products: newProducts,
+  });
 };

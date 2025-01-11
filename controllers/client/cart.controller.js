314d01ec044ff0a2ecb585eea7861a2d75224174
KhanhNguyenx <nmkhanh121203@gmail.com>
2025-01-11 23:28:25 +0700

    Cập nhật số lượng giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -72,3 +72,16 @@ module.exports.delete = async (req, res) => {
   req.flash("success", "Xóa sản phẩm khỏi giỏ hàng thành công");
   res.redirect("back");
 };
+
+//[GET] /update/:productId/:quantity
+module.exports.update = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const productId = req.params.productId;
+  const quantity = parseInt(req.params.quantity);
+  await Cart.updateOne(
+    { _id: cartId, "products.product_id": productId },
+    { $set: { "products.$.quantity": quantity } }
+  );
+  req.flash("success", "Cập nhật giỏ hàng thành công");
+  res.redirect("back");
+};

470aba4fa74d323677f4433b97d15a2616f25eb3
KhanhNguyenx <nmkhanh121203@gmail.com>
2025-01-17 00:24:11 +0700

    Lưu thêm user_id vào model Cart

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,5 +1,6 @@
 const User = require("../../models/user.model");
 const ForgotPassword = require("../../models/forgot-password.model");
+const Cart = require("../../models/cart.model");
 const md5 = require("md5");
 const generateHelper = require("../../helpers/generate");
 const sendMailHelper = require("../../helpers/sendmail");
@@ -61,7 +62,11 @@ module.exports.loginPost = async (req, res) => {
     res.redirect("back");
     return;
   }
+
+  await Cart.updateOne({ _id: req.cookies.cartId }, { user_id: user.id });
+
   res.cookie("tokenUser", user.tokenUser);
+
   res.redirect("/");
 };
 //[GET] /user/logout
@@ -161,7 +166,6 @@ module.exports.resetPasswordPost = async (req, res) => {
 };
 //[GET] /user/info
 module.exports.info = async (req, res) => {
-
   res.render("client/pages/user/info", {
     pageTitle: "Thông tin tài khoản",
   });

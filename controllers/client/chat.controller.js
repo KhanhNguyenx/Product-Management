d2c52e1c5267f3647752947b17b3be059504e942
KhanhNguyenx <nmkhanh121203@gmail.com>
2025-01-24 00:37:54 +0700

    Hiển thị data chat realtime

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -4,42 +4,46 @@ const User = require("../../models/user.model");
 // [GET] /chat/
 module.exports.index = async (req, res) => {
   const userId = res.locals.user.id;
-
+  const fullName = res.locals.user.fullName;
   // SocketIO
   _io.once("connection", (socket) => {
     // Người dùng gửi tin nhắn lên server
     socket.on("CLIENT_SEND_MESSAGE", async (content) => {
       const chat = new Chat({
         user_id: userId,
-        content: content
+        content: content,
       });
 
       await chat.save();
 
-      // Trả data ra giao diện realtime
-      // Code ở đây...
+      // Trả data về client
+      _io.emit("SERVER_SEND_MESSAGE", {
+        userId: userId,
+        fullName: fullName,
+        content: content
+      });
     });
   });
   // End SocketIO
 
   // Lấy data từ database
   const chats = await Chat.find({
-    deleted: false
+    deleted: false,
   });
 
   for (const chat of chats) {
     const infoUser = await User.findOne({
-      _id: chat.user_id
+      _id: chat.user_id,
     }).select("fullName");
 
     chat.infoUser = infoUser;
   }
 
-  console.log(chats);
+  // console.log(chats);
   // Hết Lấy data từ database
 
   res.render("client/pages/chat/index", {
     pageTitle: "Chat",
-    chats: chats
+    chats: chats,
   });
 };
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,60 @@
+
 // CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector(".chat .inner-form");
-if(formSendData) {
+if (formSendData) {
   const inputContent = formSendData.querySelector("input[name='content']");
   formSendData.addEventListener("submit", (event) => {
     event.preventDefault();
     const content = inputContent.value;
-    if(content) {
+    if (content) {
       socket.emit("CLIENT_SEND_MESSAGE", content);
       inputContent.value = "";
     }
   });
 }
 // End CLIENT_SEND_MESSAGE
+
+// SERVER_SEND_MESSAGE
+socket.on("SERVER_SEND_MESSAGE", (data) => {
+  const body = document.querySelector(".chat .inner-body");
+  const myId = document.querySelector("[my-id]").getAttribute("my-id");
+
+  const div = document.createElement("div");
+  let htmlFullName = "";
+
+  if(myId != data.userId) {
+    div.classList.add("inner-incoming");
+    htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
+  } else {
+    div.classList.add("inner-outgoing");
+  }
+
+  div.innerHTML = `
+    ${htmlFullName}
+    <div class="inner-content">${data.content}</div>
+  `;
+
+  body.appendChild(div);
+  body.scrollTop = body.scrollHeight;
+})
+// End SERVER_SEND_MESSAGE
+
+// Scroll to bottom
+const bodyChat = document.querySelector(".chat .inner-body");
+if (bodyChat) {
+  bodyChat.scrollTop = bodyChat.scrollHeight;
+}
+// End Scroll to bottom
+
+
+//Enter to send message
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.querySelector('.fa-paper-plane'); // Tìm nút với lớp "fa-paper-plane"
+
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') { // Kiểm tra nếu phím nhấn là Enter
+      button.click(); // Tự động nhấp vào nút
+    }
+  });
+});
+//End Enter to send message

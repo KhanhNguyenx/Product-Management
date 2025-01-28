58e8ea4e06ef36505027d15e8dd42eee2cfb2468
KhanhNguyenx <nmkhanh121203@gmail.com>
2025-01-28 23:51:22 +0700

    Tối ưu lại socketIO

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,48 +1,11 @@
 const Chat = require("../../models/chat.model");
 const User = require("../../models/user.model");
-
-const uploadToCloundinary = require("../../helpers/uploadToCloundinary");
+const chatSocket = require("../../sockets/client/chat.socket");
 // [GET] /chat/
 module.exports.index = async (req, res) => {
-  const userId = res.locals.user.id;
-  const fullName = res.locals.user.fullName;
+
   // SocketIO
-  _io.once("connection", (socket) => {
-    // Người dùng gửi tin nhắn lên server
-    socket.on("CLIENT_SEND_MESSAGE", async (data) => {
-      let images = [];
-      for (const imageBuffer of data.images) {
-        const link = await uploadToCloundinary(imageBuffer);
-        images.push(link);
-      }
-
-      // Lưu vào database
-      const chat = new Chat({
-        user_id: userId,
-        content: data.content,
-        images: images,
-      });
-
-      await chat.save();
-
-      // Trả data về client
-      _io.emit("SERVER_SEND_MESSAGE", {
-        userId: userId,
-        fullName: fullName,
-        content: data.content,
-        images: images,
-      });
-    });
-    //Typing
-    socket.on("CLIENT_SEND_TYPING", async (type) => {
-      socket.broadcast.emit("SERVER_RETURN_TYPING", {
-        userId: userId,
-        fullName: fullName,
-        type: type,
-      });
-    });
-    //End Typing
-  });
+  chatSocket(res);
   // End SocketIO
 
   // Lấy data từ database
---
+++ sockets/client/chat.socket.js
@@ -0,0 +1,44 @@
+const uploadToCloundinary = require("../../helpers/uploadToCloundinary");
+const Chat = require("../../models/chat.model");
+
+module.exports = (res) => {
+  const userId = res.locals.user.id;
+  const fullName = res.locals.user.fullName;
+
+  _io.once("connection", (socket) => {
+    // Người dùng gửi tin nhắn lên server
+    socket.on("CLIENT_SEND_MESSAGE", async (data) => {
+      let images = [];
+      for (const imageBuffer of data.images) {
+        const link = await uploadToCloundinary(imageBuffer);
+        images.push(link);
+      }
+
+      // Lưu vào database
+      const chat = new Chat({
+        user_id: userId,
+        content: data.content,
+        images: images,
+      });
+
+      await chat.save();
+
+      // Trả data về client
+      _io.emit("SERVER_SEND_MESSAGE", {
+        userId: userId,
+        fullName: fullName,
+        content: data.content,
+        images: images,
+      });
+    });
+    //Typing
+    socket.on("CLIENT_SEND_TYPING", async (type) => {
+      socket.broadcast.emit("SERVER_RETURN_TYPING", {
+        userId: userId,
+        fullName: fullName,
+        type: type,
+      });
+    });
+    //End Typing
+  });
+};

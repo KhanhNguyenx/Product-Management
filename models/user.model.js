9448faa97c64c2cd3ea186947072fcdc144c1dbd
KhanhNguyenx <nmkhanh121203@gmail.com>
2025-02-02 12:37:15 +0700

    Tối ưu lại script trong trang default

--- models/user.model.js
+++ models/user.model.js
@@ -16,6 +16,14 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "active",
     },
+    acceptFriends: Array, //lời mời gửi đi
+    requestFriends: Array, //lời mời nhận được
+    friendsList: [ //danh sách bạn bè
+      {
+        user_id: String,
+        room_chat_id: String
+      }
+    ],
     deleted: {
       type: Boolean,
       default: false,

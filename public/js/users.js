b36c5f6cb643395c689c5d9465472d4a16f27c77
KhanhNguyenx <nmkhanh121203@gmail.com>
2025-02-12 13:06:05 +0700

    Sửa lỗi cập nhật số lượng khi yêu cầu kết bạn

--- public/js/users.js
+++ public/js/users.js
@@ -1,39 +1,38 @@
+//Chức năng gửi yêu cầu kết bạn
+const listbuttonAddFriend = document.querySelectorAll("[btn-add-friend]");
+listbuttonAddFriend.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.closest(".box-user").classList.add("add");
 
-//Chức năng gửi yêu cầu kết bạn 
-const listbuttonAddFriend = document.querySelectorAll('[btn-add-friend]');
-listbuttonAddFriend.forEach(button => {
-    button.addEventListener('click', () => {
-      button.closest('.box-user').classList.add("add")
+    const userId = button.getAttribute("btn-add-friend");
 
-      const userId = button.getAttribute('btn-add-friend');
-
-      socket.emit('CLIENT_ADD_FRIEND', userId);
-    });
+    socket.emit("CLIENT_ADD_FRIEND", userId);
+  });
 });
-//Hết chức năng gửi yêu cầu kết bạn 
+//Hết chức năng gửi yêu cầu kết bạn
 
-//Chức năng hủy yêu cầu kết bạn 
-const listbuttonCancelFriend = document.querySelectorAll('[btn-cancel-friend]');
-listbuttonCancelFriend.forEach(button => {
-    button.addEventListener('click', () => {
-      button.closest('.box-user').classList.remove("add")
+//Chức năng hủy yêu cầu kết bạn
+const listbuttonCancelFriend = document.querySelectorAll("[btn-cancel-friend]");
+listbuttonCancelFriend.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.closest(".box-user").classList.remove("add");
 
-      const userId = button.getAttribute('btn-cancel-friend');
+    const userId = button.getAttribute("btn-cancel-friend");
 
-      socket.emit('CLIENT_CANCEL_FRIEND', userId);
-    });
+    socket.emit("CLIENT_CANCEL_FRIEND", userId);
+  });
 });
-//Hết chức năng hủy yêu cầu kết bạn 
+//Hết chức năng hủy yêu cầu kết bạn
 
 // Từ chối kết bạn
 const listbuttonRefuseFriend = document.querySelectorAll("[btn-refuse-friend]");
-if(listbuttonRefuseFriend.length > 0) {
-  listbuttonRefuseFriend.forEach(button => {
+if (listbuttonRefuseFriend.length > 0) {
+  listbuttonRefuseFriend.forEach((button) => {
     button.addEventListener("click", () => {
       button.closest(".box-user").classList.add("refuse");
 
       const userId = button.getAttribute("btn-refuse-friend");
-      
+
       socket.emit("CLIENT_REFUSE_FRIEND", userId);
     });
   });
@@ -42,13 +41,13 @@ if(listbuttonRefuseFriend.length > 0) {
 
 // Chấp nhận kết bạn
 const listbuttonAcceptFriend = document.querySelectorAll("[btn-accept-friend]");
-if(listbuttonAcceptFriend.length > 0) {
-  listbuttonAcceptFriend.forEach(button => {
+if (listbuttonAcceptFriend.length > 0) {
+  listbuttonAcceptFriend.forEach((button) => {
     button.addEventListener("click", () => {
       button.closest(".box-user").classList.add("accepted");
 
       const userId = button.getAttribute("btn-accept-friend");
-      
+
       socket.emit("CLIENT_ACCEPT_FRIEND", userId);
     });
   });
@@ -56,8 +55,13 @@ if(listbuttonAcceptFriend.length > 0) {
 // Hết Chấp nhận kết bạn
 
 // SERVER_RETURN_LENGTH_ACCEPT_FRIEND
-socket.on("SERVER_RETURN_LENGTH_ACCEPT_FRIEND", (data) => {
-  const badgeUsersAccept = document.querySelector(`[badge-users-accept="${data.userId}"]`);
-  badgeUsersAccept.innerHTML = data.lengthAcceptFriends;
-});
+const badgeUsersAccept = document.querySelector(`[badge-users-accept]`);
+if (badgeUsersAccept) {
+  const userId = badgeUsersAccept.getAttribute("badge-users-accept");
+  if (userId === data.userId) {
+    socket.on("SERVER_RETURN_LENGTH_ACCEPT_FRIEND", (data) => {
+      badgeUsersAccept.innerHTML = data.lengthAcceptFriends;
+    });
+  }
+}
 // End SERVER_RETURN_LENGTH_ACCEPT_FRIEND

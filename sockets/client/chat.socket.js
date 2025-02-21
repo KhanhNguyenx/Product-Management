16a13d4738378cb632bd53973272f7c21c41dc4f
KhanhNguyenx <nmkhanh121203@gmail.com>
2025-02-21 14:20:30 +0700

    Nhắn tin theo phòng chat 2 người

--- sockets/client/chat.socket.js
+++ sockets/client/chat.socket.js
@@ -1,11 +1,13 @@
 const uploadToCloundinary = require("../../helpers/uploadToCloundinary");
 const Chat = require("../../models/chat.model");
 
-module.exports = (res) => {
+module.exports = (req, res) => {
   const userId = res.locals.user.id;
   const fullName = res.locals.user.fullName;
-
+  const roomChatId = req.params.roomChatId;
   _io.once("connection", (socket) => {
+    socket.join(roomChatId);
+
     // Người dùng gửi tin nhắn lên server
     socket.on("CLIENT_SEND_MESSAGE", async (data) => {
       let images = [];
@@ -17,6 +19,7 @@ module.exports = (res) => {
       // Lưu vào database
       const chat = new Chat({
         user_id: userId,
+        room_chat_id: roomChatId,
         content: data.content,
         images: images,
       });
@@ -24,7 +27,7 @@ module.exports = (res) => {
       await chat.save();
 
       // Trả data về client
-      _io.emit("SERVER_SEND_MESSAGE", {
+      _io.to(roomChatId).emit("SERVER_SEND_MESSAGE", {
         userId: userId,
         fullName: fullName,
         content: data.content,
@@ -33,7 +36,7 @@ module.exports = (res) => {
     });
     //Typing
     socket.on("CLIENT_SEND_TYPING", async (type) => {
-      socket.broadcast.emit("SERVER_RETURN_TYPING", {
+      socket.broadcast.to(roomChatId).emit("SERVER_RETURN_TYPING", {
         userId: userId,
         fullName: fullName,
         type: type,
